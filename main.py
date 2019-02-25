a8a5ac2833c57a01f842f6e039744754cd96bd56
Alysha O'Hara <Alysha.Griesemer@gmail.com>
2019-02-25 14:55:46 -0600

    Display all posts or an individual post

--- main.py
+++ main.py
@@ -16,8 +16,31 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # set to Text so there is not a character limit
     title = db.Column(db.Text) # this is the title
-    post = db.Column(db.Text) # this is the post
+    body = db.Column(db.Text) # this is the post
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
+
+
+# display individual blog entries or all blog posts
+
+@app.route('/blog')
+def display_blog_entries():
+    entry_id = request.args.get('id')
+    if (entry_id):
+        entry = Entry.query.get(entry_id)
+        return render_template('single_entry.html', title="Blog Post", entry=entry)
+    else:
+        # display every existing blog entry
+        sort = request.args.get('sort')
+        # displays newest first
+        if (sort=="newest"):
+            all_entries = Entry.query.order_by(Entry.created.desc()).all()
+        else:
+        # just shows all entries
+            all_entries = Entry.query.all()
+        return render_template('all_entries.html', title= "All Blog Posts", all_entries=all_entries)
+        
+
+
